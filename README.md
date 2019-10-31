64e1998bb3088eb974658a846cae3344d1611640
AlexG <xelaflash@gmail.com>
2019-10-31 11:16:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Middleman Template
+Template with Bootsrap 4 + font awesome and webpack
+
+
+## Authors
+Based on middleman template made by [B.Calin](https://github.com/benoitcalin/middleman-bootstrap4-template)  and *Initial work* - [LeWagon](https://github.com/lewagon/middleman-template)"
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      jquery: 'jquery'
+    })
+  ],
+  entry: {
+    main: './source/assets/javascripts/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|svg|eot|ttf|woff|woff2)$/i,
+        use: ['file-loader']
+      }
+    ]
+  }
+};
