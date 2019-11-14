654f2ed6bbaa882803c9704d4397682d15fcf767
Alexandre Gros <xelaflash@gmail.com>
2019-11-14 11:08:16 -0400

    outline for keyboard users

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -7,9 +7,12 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 // components JS files
 import { initMapbox } from './components/init_mapbox';
 import { toogleNav } from './components/mobile_nav';
+import { handleFirstTab } from './components/keyboard_focus';
 import './components/form_validation';
 import './components/back_to_top';
 
+// outline for keyboard user on focus
+window.addEventListener('keydown', handleFirstTab);
 
 // Sidenav
 const sidenavTrigger = document.getElementById("sidenav-trigger");
