1a8f773c5a82811bc030cd2807a798a0baf098f5
Alexandre Gros <xelaflash@gmail.com>
2019-11-15 10:10:49 -0400

    start styling inde

--- config.rb
+++ config.rb
@@ -19,7 +19,7 @@ activate :external_pipeline,
   activate :blog do |blog|
     blog.permalink = 'actualites/{title}.html'
     blog.sources = 'posts/{year}-{month}-{day}-{title}.html'
-    blog.layout = 'blog-layout'
+    # blog.layout = 'blog-layout'
   end
 
 # ------ Uncomment and adapt if you want to use i18n ------
