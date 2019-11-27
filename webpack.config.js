15f81412664f3ae031324f2fe93b4c436b422f09
Alexandre Gros <xelaflash@gmail.com>
2019-11-27 10:58:13 -0400

    small config change

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.m?js$/,
-        exclude: /(node_modules)/,
+        test: /\.js$/,
+        exclude: /node_modules/,
         loader: 'babel-loader'
       },
       {
